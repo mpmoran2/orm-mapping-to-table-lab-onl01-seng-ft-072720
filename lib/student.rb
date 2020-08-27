cbac7197ccebe23183f6c26ebae0d35b84ff76f2
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-27 17:39:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,10 +29,10 @@ class Student
   end 
  
   def save
-    sql = <<-SQL
+  sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
-      SQL
+    SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
 
